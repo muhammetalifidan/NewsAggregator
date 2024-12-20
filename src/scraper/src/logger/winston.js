275893d0f8613f976123a940987f42ed01f57a48
mhmmtalifdn <mhmmtalifdn@gmail.com>
2024-12-20 23:06:51 +0300

    feat(scraper): Add logger setup for the scraper

---
+++ src/scraper/src/logger/winston.js
@@ -0,0 +1,26 @@
+import * as winston from 'winston';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const loggerConfig = {
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({
+            filename: path.join(__dirname, '../../logs/error.log'),
+            level: 'error'
+        }),
+        new winston.transports.File({
+            filename: path.join(__dirname, '../../logs/combined.log')
+        })
+    ]
+};
+
+export const logger = winston.createLogger(loggerConfig);
